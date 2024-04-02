9383aa31edbd8bf4e53097b11038f652a61cc4a8
sebassg7 <sebastlopez@unicauca.edu.co>
2024-04-02 13:54:35 -0500

    Diseño de la pantalla del chat

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,1 +1,3 @@
+export * from './bubbles/GptMessage';
+export * from './bubbles/MyMessage';
 export * from './sidebar/SidebarMenuItem';
--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,5 +1,16 @@
+import { MyMessage } from '../../components';
+import { GptMessage } from '../../components';
 export const OrthographyPage = () => {
   return (
-    <div>OrthographyPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2"></div>
+          
+          {/* Bienvenida */}
+          <GptMessage text="¿Cómo estás? Encantado de ayudarte a corregir tus textos"/>
+          <MyMessage text="Hola Mundo"/>
+
+      </div>
+    </div>
   )
 }
